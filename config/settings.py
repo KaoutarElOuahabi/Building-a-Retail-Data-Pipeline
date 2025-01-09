ca5f090e9115008c276bc57bc6e63e556af2f5a5
samira <kaoutar.elouahabi.de@gmail.com>
2025-01-09 18:41:30 +0100

    Complete Retail Data Pipeline project: ETL process, data integration, transformations, testing, and documentation.

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+# Purpose: Store all configuration settings in one place (e.g., database credentials, file paths).
+
+# Define Configuration (settings.py)
+DB_CONFIG = {
+    "host": "",  # Replace with your database host
+    "port": 5324 ,         # Replace with your database port
+    "database": "",  # Replace with your database name
+    "user": "",      # Replace with your username
+    "password": ""   # Replace with your password
+}
+
+
+INPUT_FILE = "data/input/extra_data.parquet"
+OUTPUT_FILE = "data/output/transformed_data.csv"
